660f472086fc98bee25b53db97212bec97d3e1b6
Biswadeep Bhattacharjee <83005362+biswa290701@users.noreply.github.com>
2022-05-07 15:16:24 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which language forms the skeleton of web development?",
+        answer: "HTML",
+        options: [
+            "JavaScript",
+            "CSS",
+            "HTML",
+            "Java"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which language looks ater the design of a web page?",
+        answer: "CSS",
+        options: [
+            "CSS",
+            "HTML",
+            "JavaScript",
+            "C#"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the full form of SQL?",
+        answer: "Structured Query Language",
+        options: [
+            "Systemic Question Language",
+            "Systemic Query Language",
+            "Structured Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which language looks after the manipulation of data in a database?",
+        answer: "DML",
+        options: [
+            "DML",
+            "TCL",
+            "DDL",
+            "DCL"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the coding language that the game engine Unity uses?",
+        answer: "C#",
+        options: [
+            "C++",
+            "C#",
+            "Java",
+            "Python"
+        ]
+    }
+];
